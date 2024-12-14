681af35982289d9bf2f723e32b343fa52b678e6d
DaverSVK <david.szepvolgyi@gmail.com>
2024-12-14 21:42:02 +0100

    Refactored hardware

--- full2.py
+++ full2.py
@@ -17,10 +17,38 @@ import adafruit_bh1750
 import adafruit_ahtx0
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+
+# Local LIBS
 import sensors_periferal
+import hardware_control
 
 # Yolo detection
 from ultralytics import YOLO
+
+# Functions
+def authenticate():
+    response = auth.sign_in_with_email_and_password("piunit@unit.sk","12345678")
+    token = response['idToken']
+    print(token)
+    return token
+
+def storePicture(x,now):
+    filename1 = "cam-" + now.strftime("%Y-%m-%d_%H.%M.%S.jpg")
+    filename = "/home/pi/Desktop/"+filename1
+    cap = cv2.VideoCapture(0)
+    # Perform inference
+    ret,frame = cap.read()
+    out = cv2.imwrite(filename, frame)
+    output = model.predict(source=filename, conf=0.5, save=False)
+    outPlot = output[0].plot()
+    cv2.imwrite(filename,outPlot)
+    print(filename +" saved")
+    storage.child(filename1).put(filename)
+    print("Image sent")
+    os.remove(filename)
+    print("File Removed")
+    return x
+#___________________________________________________
 
 # Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -28,14 +56,10 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1015(i2c)
 ads.gain = 1
-#Lux sensor
-sensor_bh1750, is_bh1750_availible = sensors_periferal.initBH1750(i2c)
 
-#temp/bar sensor
-sensor_bmp280, is_bmp280_availible = sensors_periferal.initBMP280(i2c)
-
-#temp/bar sensor
-sensor_ahtx0, is_ahtx0_availible = sensors_periferal.initAHTx0(i2c)
+sensor_bh1750, is_bh1750_availible = sensors_periferal.initBH1750(i2c) # Lux sensor
+sensor_bmp280, is_bmp280_availible = sensors_periferal.initBMP280(i2c) # temp/bar sensor
+sensor_ahtx0, is_ahtx0_availible = sensors_periferal.initAHTx0(i2c) # temp/bar sensor
 
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
@@ -91,124 +115,13 @@ now = datetime.now()
 nextLog = now
 lastLog = now
 
-def authenticate():
-    response = auth.sign_in_with_email_and_password("piunit@unit.sk","12345678")
-    token = response['idToken']
-    print(token)
-    return token;
-
-def getDataValues(chan,chan1,chan2,chan3,sensor_bh1750,sensor_bmp280,sensor_ahtx0,now):
-    
-    lux = 0.0
-    temperature = 0.0
-    pressure = 0.0
-    altitude = 0.0
-    temperature2 = 0.0
-    humidity = 0.0
-    
-    if(is_bh1750_availible):
-        # Sensor svetla   
-        lux = round(sensor_bh1750.lux, 2)
-    if(is_bmp280_availible):
-        # Sensor press/bar   
-        temperature = round(sensor_bmp280.temperature, 2)
-        pressure = round(sensor_bmp280.pressure, 2)
-        altitude = round(sensor_bmp280.altitude, 2)
-    if(is_ahtx0_availible):
-        # Sensor TEMP up
-        temperature2 = round(sensor_ahtx0.temperature, 2)
-        humidity = round(sensor_ahtx0.relative_humidity, 2)
-
-    # dole pravý
-    water = int(100-(100/1.366*(chan.voltage-0.806)))
-    # hore lavý
-    water2 = int(100-(100/1.358*(chan1.voltage-0.796)))
-    # horny pravý
-    water3 = int(100-(100/1.348*(chan2.voltage-0.844)))
-    # dolny lavý
-    water4 = int(100-(100/1.398*(chan3.voltage-0.768)))
-    data={"date": str(now),"water": water, "water2": water2, "water3": water3, "water4": water4, "lux": lux, "temperature": temperature,"temperature2": temperature2, "pressure": pressure,"humidity": humidity, "altitude": altitude}
-    return data;
-
-def startWatering(watering):
-    if(int(watering) > 0):
-        print("Watering")
-        GPIO.output(5, GPIO.LOW)
-        GPIO.output(6, GPIO.HIGH)
-        sleep(2)
-        GPIO.output(6, GPIO.LOW)
-        GPIO.output(5, GPIO.LOW)
-        sleep(5)
-        print("pump off")
-    return True;
-
-def toggleLight(wantedState):
-    servoLight = GPIO.PWM(12,50)
-    servoLight.start(0)
-    if(wantedState==True):
-        servoLight.ChangeDutyCycle(6.6)
-        sleep(2)
-        lightState = False;
-
-    else:
-        servoLight.ChangeDutyCycle(5)
-        sleep(2)
-        lightState = True;
-
-    servoLight.ChangeDutyCycle(0)
-    sleep(0.5)
-    servoLight.stop()
-    return lightState;
-
-def toggleFan(wantedState):
-    if(wantedState == True):
-        fan.ChangeDutyCycle(40)
-        fanStateChange = True
-    else:
-        fan.ChangeDutyCycle(0)
-        fanStateChange = False
-    return True
-
-def toggleHeating(tempSetpoint,sensor_bmp280):
-    tempCurrent = round(sensor_bmp280.temperature, 2)
-    print(tempCurrent)
-    if(tempCurrent > float(tempSetpoint)+2):
-        GPIO.output(26, GPIO.LOW)
-        GPIO.output(19, GPIO.LOW)
-        print("heating off")
-    if (tempCurrent < float(tempSetpoint)-2):
-        GPIO.output(19, GPIO.LOW)
-        GPIO.output(26, GPIO.HIGH)
-        print("heating on")
-    return True;
-#___________________________________________________
-    
-    
-def storePicture(x,now):
-    filename1 = "cam-" + now.strftime("%Y-%m-%d_%H.%M.%S.jpg")
-    filename = "/home/pi/Desktop/"+filename1
-    cap = cv2.VideoCapture(0)
-    # Perform inference
-    ret,frame = cap.read()
-    out = cv2.imwrite(filename, frame)
-    output = model.predict(source=filename, conf=0.5, save=False)
-    outPlot = output[0].plot()
-    cv2.imwrite(filename,outPlot)
-    print(filename +" saved")
-    storage.child(filename1).put(filename)
-    print("Image sent")
-    os.remove(filename)
-    print("File Removed")
-    return x;
-
-url2="https://greenhouseapp-a928f-default-rtdb.firebaseio.com/settings"
-token =authenticate()
+token = authenticate()
 
 while True:
-    
     # try:
     r= requests.get(url2 + ".json?auth="+token)
     data=r.json()
+    
     x=data['sampling_time']
     timeStart = data['light_start']
     timeDur = data['light_duration']
@@ -216,29 +129,32 @@ while True:
     watering = data['watering']
     fanSett = data['fan_state']
     print(fanSett)
+    
     now = datetime.now()
     begin = now.replace(hour=int(timeStart), minute=0, second=0, microsecond=0)
     stop = now.replace(hour=(int(timeStart)+int(timeDur)), minute=0, second=0, microsecond=0)
 #        print(stop)
 #         print(begin)
     if int(fanSett) == 1:
-        toggleFan(True)
+        hardware_control.toggleFan(True,fan)
     if int(fanSett) == 0:
-        toggleFan(False)
+        hardware_control.toggleFan(False,fan)
 
     if now > stop and lightState == True:
-        lightState = toggleLight(True)
+        lightState = hardware_control.toggleLight(True)
     if now < stop and now > begin and lightState == False:
-        lightState = toggleLight(False)
-    toggleHeating(temp,sensor_bmp280)
-    startWatering(watering)
+        lightState = hardware_control.toggleLight(False)
+    
+    hardware_control.toggleHeating(temp,sensor_bmp280)
+    hardware_control.startWatering(watering)
     print(x)
+    
     if now < stop and now > begin:
         if now >= nextLog:
             lastLog = now
             print(nextLog)
             store = storePicture(x,now)
-            data = getDataValues(chan,chan1,chan2,chan3,sensor_bh1750,sensor_bmp280,sensor_ahtx0,now)
+            data = sensors_periferal.getDataValues(chan,chan1,chan2,chan3,sensor_bh1750,sensor_bmp280,sensor_ahtx0,now,is_bh1750_availible,is_bmp280_availible,is_ahtx0_availible)
             requests.post(urlData + ".json", json=data)
     nextLog = lastLog + timedelta(seconds=int(x))
     # except:
--- sensors_periferal.py
+++ sensors_periferal.py
@@ -29,3 +29,46 @@ def initAHTx0(i2c):
         print("AHTx0 not detected. Check wiring and address.")
         return None, False
 
+def getDataValues(chan,chan1,chan2,chan3,sensor_bh1750,sensor_bmp280,sensor_ahtx0,now,is_bh1750_availible,is_bmp280_availible,is_ahtx0_availible):
+    
+    lux = 0.0
+    temperature = 0.0
+    pressure = 0.0
+    altitude = 0.0
+    temperature2 = 0.0
+    humidity = 0.0
+    
+    if(is_bh1750_availible):
+        # Sensor svetla   
+        lux = round(sensor_bh1750.lux, 2)
+    if(is_bmp280_availible):
+        # Sensor press/bar   
+        temperature = round(sensor_bmp280.temperature, 2)
+        pressure = round(sensor_bmp280.pressure, 2)
+        altitude = round(sensor_bmp280.altitude, 2)
+    if(is_ahtx0_availible):
+        # Sensor TEMP up
+        temperature2 = round(sensor_ahtx0.temperature, 2)
+        humidity = round(sensor_ahtx0.relative_humidity, 2)
+
+    # dole pravý
+    water = int(100-(100/1.366*(chan.voltage-0.806)))
+    # hore lavý
+    water2 = int(100-(100/1.358*(chan1.voltage-0.796)))
+    # horny pravý
+    water3 = int(100-(100/1.348*(chan2.voltage-0.844)))
+    # dolny lavý
+    water4 = int(100-(100/1.398*(chan3.voltage-0.768)))
+    data={"date": str(now),"water": water, "water2": water2, "water3": water3, "water4": water4, "lux": lux, "temperature": temperature,"temperature2": temperature2, "pressure": pressure,"humidity": humidity, "altitude": altitude}
+    return data
+
+def getBMP280(sensor_bmp280,is_bmp280_availible):
+    pressure = 0.0
+    altitude = 0.0
+    temperature2 = 0.0
+    if(is_bmp280_availible):
+        # Sensor press/bar   
+        temperature2 = round(sensor_bmp280.temperature, 2)
+        pressure = round(sensor_bmp280.pressure, 2)
+        altitude = round(sensor_bmp280.altitude, 2)
+    return temperature2
